7f09b0e50b2dd435add9aa734c729718179be759
Gethin Davies <grd.davies@gmail.com>
2021-11-05 14:26:58 +0000

    Print "ERROR" on syntax error in calc

--- R/app_server.R
+++ R/app_server.R
@@ -18,7 +18,7 @@ app_server <- function(input, output, session) {
   mod_calc_button_server("b/", "÷", updateTextInput(session, "screen", value = paste0(input$screen, "/")))
   mod_calc_button_server("bv", shiny::HTML("&radic;"), updateTextInput(session, "screen", value = paste0(input$screen, "sqrt(")))
   # Equals/Evaluate
-  mod_calc_button_server("b=", "=", updateTextInput(session, "screen", value = (interpret(input$screen))))
+  mod_calc_button_server("b=", "=", updateTextInput(session, "screen", value = tryCatch(interpret(input$screen), error = function(e){"ERROR"})))
   # Clear
   mod_calc_button_server("bC", "C", updateTextInput(session, "screen", value = ""))
 }
