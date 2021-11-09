22529fc4b5866834b0eb96aaf665d6a8c25963ca
Gethin Davies <grd.davies@gmail.com>
2021-11-09 14:55:22 +0000

    Change button IDs to valid HTML id attr values
    +, =, / etc are not valid HTML id attribute values, which caused some errors with jQuery.
    
    Updated to decouple the button label and button ids for these problem values

--- R/mod_calculator.R
+++ R/mod_calculator.R
@@ -16,11 +16,11 @@ mod_calculator_ui <- function(id) {
       # Buttonpad Layout
       purrr::map(
         list(
-          list("C", "(", ")", "/"),
-          list(7, 8, 9, "*"),
-          list(4, 5, 6, "-"),
-          list(1, 2, 3, "+"),
-          list("v", 0, ".", "=")
+          list("C", "lparen", "rparen", "slash"),
+          list(7, 8, 9, "mult"),
+          list(4, 5, 6, "minus"),
+          list(1, 2, 3, "plus"),
+          list("v", 0, "point", "eq")
         ),
         ~ {
           div(
@@ -41,22 +41,33 @@ mod_calculator_server <- function(id) {
     ns <- session$ns
     # Buttons that write what they say on the tin
     purrr::map(
-      c(0:9, "*", "+", "-", ")", "(", "."),
+      c(0:9),
       ~ mod_calc_button_server(
         id = paste0("b", .),
         label = .,
         callback = updateTextInput(session, "screen", value = paste0(input$screen, .))
       )
     )
-    # Divide button
-    mod_calc_button_server("b/", "\u00F7", updateTextInput(session, "screen", value = paste0(input$screen, "/")))
+    # Buttons that need a different id to what they write to be valid html
+    purrr::map2(
+      c("plus", "mult", "minus", "rparen", "lparen", "point"),
+      c("+", "*", "-", ")", "(", "."),
+      ~ mod_calc_button_server(
+        id = paste0("b", .x),
+        label = .y,
+        callback = updateTextInput(session, "screen", value = paste0(input$screen, .y))
+      )
+    )
+    # Divide/slash
+    mod_calc_button_server("bslash", "\u00F7", updateTextInput(session, "screen", value = paste0(input$screen, "/")))
+    # Sqrt
     mod_calc_button_server("bv", HTML("&radic;"), updateTextInput(session, "screen", value = paste0(input$screen, "sqrt(")))
-    # Equals/Evaluate
-    mod_calc_button_server("b=", "=", updateTextInput(session, "screen", value = tryCatch(interpret(input$screen), error = function(e) {
-      "ERROR"
-    })), class = "btn-warning")
     # Clear
     mod_calc_button_server("bC", "C", updateTextInput(session, "screen", value = ""))
+    # Equals/Evaluate
+    mod_calc_button_server("beq", "=", updateTextInput(session, "screen", value = tryCatch(interpret(input$screen), error = function(e) {
+      "ERROR"
+    })), class = "btn-warning")
   })
 }
 
